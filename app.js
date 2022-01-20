a95981309f0094cd051cf2fe4656f934c792090f
nezjutsu <seankral9@gmail.com>
2022-01-19 23:23:26 -0500

    validate users answers on required questions

--- app.js
+++ app.js
@@ -15,12 +15,28 @@ const promptUser = () => {
             {
                 type: 'input',
                 name: 'name',
-                message: 'What is your name?'
+                message: 'What is your name?',
+                validate: nameInput => {
+                    if (nameInput) {
+                        return true;
+                    } else {
+                        console.log('Plase enter your name!');
+                        return false;
+                    }
+                }
             },
             {
                 type: 'input',
                 name: 'github',
-                message: 'Enter your GitHub username:'
+                message: 'Enter your GitHub username. (Required):',
+                validate: nameInput => {
+                    if (nameInput) {
+                        return true;
+                    } else {
+                        console.log('Please enter your GitHub username!');
+                        return false;
+                    }
+                }
             },
             {
                 type: 'input',
@@ -46,12 +62,28 @@ const promptProject = portfolioData => {
             {
                 type: 'input',
                 name: 'name',
-                message: 'What is the name of your project?'
+                message: 'What is the name of your project? (Required):',
+                validate: nameInput => {
+                    if (nameInput) {
+                        return true;
+                    } else {
+                        console.log('Please enter your Project Name!');
+                        return false;
+                    }
+                }
             },
             {
                 type: 'input',
                 name: 'description',
-                message: 'Provide a description of the project (Required):'
+                message: 'Provide a description of the project (Required):',
+                validate: nameInput => {
+                    if (nameInput) {
+                        return true;
+                    } else {
+                        console.log('Plase enter a description for your Project!');
+                        return false;
+                    }
+                }
             },
             {
                 type: 'checkbox',
@@ -62,7 +94,15 @@ const promptProject = portfolioData => {
             {
                 type: 'input',
                 name: 'link',
-                message: 'Enter the GitHub link to your project. (Required):'
+                message: 'Enter the GitHub link to your project. (Required):',
+                validate: nameInput => {
+                    if (nameInput) {
+                        return true;
+                    } else {
+                        console.log('Please enter the link for your GitHub Project!');
+                        return false;
+                    }
+                }
             },
             {
                 type: 'confirm',
