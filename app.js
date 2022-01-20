3579d1f6fd0130ca318a6ec190434cdbeb97aeb0
nezjutsu <seankral9@gmail.com>
2022-01-19 20:02:08 -0500

    created separate js file to handle webpage generation logic, refactor app.js file

--- app.js
+++ app.js
@@ -1,16 +1,11 @@
-const profileDataArgs = process.argv.slice(2, process.argv.length);
-console.log(profileDataArgs);
+const fs = require('fs');
+const generatePage = require('./src/page-template.js');
 
-const printProfileData = profileDataArr => {
-    //this ...
-    for (let i = 0; i < profileDataArr.length; i++) {
-        console.log(profileDataArr[i]);
-    }
+const profileDataArgs = process.argv.slice(2);
+const [name, github] = profileDataArgs;
 
-    console.log('==========');
+fs.writeFile('./index.html', generatePage(name, github), err => {
+    if (err) throw new Error(err);
 
-    // is the same as this
-    profileDataArr.forEach((profileItem) => console.log(profileItem));
-};
-
-printProfileData(profileDataArgs)
+    console.log('Portfolio complete! Check out index.html to see the output!')
+});
